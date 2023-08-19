fba39265d677f72106fef17affe37508e58e2be7
Divyashree Iyer <diyer171100@gmail.com>
2023-08-18 21:44:04 -0700

    changed the settings info

--- run_tests.py
+++ run_tests.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
         klass = build_test_class(name)
         suite.addTest(klass(TestMetaclass.test_name(name)))
 
-    with open('/autograder/results/results.json', 'w') as f:
+    with open('results/results.json', 'w') as f:
         JSONTestRunner(visibility='visible', stream=f).run(suite)
